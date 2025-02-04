d107ba2df8fc0db19972ae5477fdd776b2f3f272
piyushsingal3 <157308982+piyushsingal3@users.noreply.github.com>
2025-02-04 14:41:30 +0530

    Added Closest Server Distance and Public IPv6

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,83 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
 
+
 class NetworkInfo {
+static Future<Map<String, int>> loadServersFromJson(String filePath) async {
+  try {
+    print('Looking for JSON file at: ${Directory.current.path}/${filePath}');
+
+    final file = File(filePath);
+    if (!await file.exists()) {
+      print('Error: JSON file not found.');
+      return {};
+    }
+
+    String content = await file.readAsString();
+    Map<String, dynamic> jsonData = jsonDecode(content);
+
+    Map<String, int> servers = {};
+    if (jsonData['servers'] != null && jsonData['servers'] is List) {
+      for (var server in jsonData['servers']) {
+        if (server is Map<String, dynamic> && 
+            server['publicIpv4'] is String && 
+            server['listeningPort'] is int) {
+          
+          String ip = server['publicIpv4'].trim();
+          int port = server['listeningPort'];
+          servers[ip] = port;
+        }
+      }
+    }
+    return servers;
+  } catch (e) {
+    print('Error reading JSON file: \$e');
+    return {};
+  }
+}
+
+static Future<String> findClosestServer(Map<String, int> servers) async {
+  String closestServer = '';
+  int minTime = 99999999;
+
+  for (var entry in servers.entries) {
+    String server = entry.key;
+    int port = entry.value;
+
+    try {
+      Stopwatch stopwatch = Stopwatch()..start();
+      Socket socket = await Socket.connect(server, port, timeout: Duration(seconds: 3));
+      stopwatch.stop();
+      socket.destroy();  // Close connection
+
+      int elapsedTime = stopwatch.elapsedMilliseconds;
+      print('Server ${server} on port ${port} responded in ${elapsedTime} ms');
+
+      if (elapsedTime < minTime) {
+        minTime = elapsedTime;
+        closestServer = '${server}:${port}';
+      }
+    } catch (e) {
+      print('Failed to connect to ${server} on port ${port}: ${e}');
+    }
+  }
+
+  return closestServer;
+}
+
   Future<void> checkNAT() async {
     try {
       await printIps();
-      
-      Map< String , dynamic> public=await getPublicIP('stun.l.google.com', 19302);
-      String publicIp=public['publicIP'];
-      bool isBehindNAT = await checkIfBehindNAT(publicIp);
+      Map< String , dynamic> publicIPv4=await getPublicIPv4('stun.l.google.com', 19302);
+      Map< String , dynamic> publicIPv6=await getPublicIPv6('stun.l.google.com', 19302);
+      String publicIpv4=publicIPv4['publicIP'];
+      String publicIpv6=publicIPv6['publicIP'];
+      bool isBehindNAT = await checkIfBehindNAT(publicIpv4);
     } catch (e) {
       print("Error: $e");
     }
   }
+
 
   Future<List<List<dynamic>>> printIps() async {
     List<List<dynamic>> activeIPs = [];
@@ -42,20 +107,23 @@ class NetworkInfo {
                 String ipType =
                     address.type == InternetAddressType.IPv6 ? "IPv6" : "IPv4";
                 String isPrivate = isPrivateIP(ip) ? "Yes" : "No";
-                Map< String , dynamic> public=await getPublicIP('stun.l.google.com', 19302);
-                String? publicIP=public['publicIP'];
-                int? publicPort=public['publicPort'];
-                bool? behindNAT = publicIP != null ? await checkIfBehindNAT(publicIP) : null;
-                String? natType = publicIP != null && behindNAT != null
+               
+                Map< String , dynamic> publicIPv4=await getPublicIPv4('stun.l.google.com', 19302);
+                Map< String , dynamic> publicIPv6=await getPublicIPv6('stun.l.google.com', 19302);
+                String? public4=publicIPv4['publicIP'];
+                String? public6=publicIPv6['publicIP'];
+                int? publicPort=publicIPv4['publicPort'];
+                bool? behindNAT = public4 != null ? await checkIfBehindNAT(public4) : null;
+                String? natType = public6 != null && behindNAT != null
                     ? await determineNATType()
-                    : null;
+                    : "";
                 int? isBehindNAT=0;
                 if(behindNAT==true){
                     isBehindNAT=1;
                 }
 
-                // [type, address, private, publicIP,publicPort isBehindNAT, natType]
-                activeIPs.add([ipType, ip, isPrivate, publicIP,publicPort, isBehindNAT, natType]);
+                // [type, address, private, publicIP,publicPort ,isBehindNAT, natType]
+                activeIPs.add([ipType, ip, isPrivate, public4,publicPort, isBehindNAT, natType,public6]);
 
 
               }
@@ -103,7 +171,7 @@ class NetworkInfo {
     return false; 
   }
 
-Future<Map<String, dynamic>> getPublicIP(String stunServer, int stunPort) async {
+Future<Map<String, dynamic>> getPublicIPv4(String stunServer, int stunPort) async {
   final socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
 
   final transactionId = List<int>.generate(12, (i) => i);
@@ -165,6 +233,66 @@ Future<Map<String, dynamic>> getPublicIP(String stunServer, int stunPort) async 
     'publicIP': publicIP,
     'publicPort': publicPort,
   };
+}
+Future<Map<String, dynamic>> getPublicIPv6(String stunServer, int stunPort) async {
+  final socket = await RawDatagramSocket.bind(InternetAddress.anyIPv6, 0);
+
+  final transactionId = List<int>.generate(12, (i) => i);
+  final stunMessage = Uint8List.fromList([
+    0x00, 0x01,
+    0x00, 0x00,
+    0x21, 0x12, 0xA4, 0x42,
+    ...transactionId,
+  ]);
+
+  final stunServerAddress = (await InternetAddress.lookup(stunServer))
+      .where((addr) => addr.type == InternetAddressType.IPv6)
+      .toList();
+
+  if (stunServerAddress.isEmpty) {
+    
+    return {'publicIP': "", 'publicPort': null};
+  }
+
+  final stunServerIP = stunServerAddress.first;
+
+  socket.send(stunMessage, stunServerIP, stunPort);
+
+  String? publicIP;
+  int? publicPort;
+
+  await for (var event in socket) {
+    if (event == RawSocketEvent.read) {
+      final datagram = socket.receive();
+      if (datagram != null) {
+        final response = datagram.data;
+        if (response.length > 20) {
+          final addressFamily = response[25];
+          if (addressFamily == 0x02) { // IPv6
+  publicPort = (response[26] << 8 | response[27]) ^
+      (transactionId[0] << 8 | transactionId[1]);
+
+  final ip = List.generate(16, (i) => 
+    (response[28 + i] ^ transactionId[i % transactionId.length])
+    .toRadixString(16).padLeft(2, '0'))
+    .join(':');
+  publicIP = ip.replaceAllMapped(
+    RegExp(r'(:0{1,3})+'), 
+    (match) => ':'
+  );
+} else {
+            print('Received a non-IPv6 response.');
+          }
+        } else {
+          print('Invalid STUN response.');
+        }
+      }
+      break;
+    }
+  }
+
+  socket.close();
+  return {'publicIP': publicIP, 'publicPort': publicPort};
 }
  Future<String> determineNATType() async {
     try {
@@ -326,6 +454,7 @@ void main() async {
   await networkInfo.determineNATType();
   
   List<List<dynamic>> activeIPs = await networkInfo.printIps();
+  
 
   if (activeIPs.isEmpty) {
     print('No active IPs found.');
@@ -333,9 +462,18 @@ void main() async {
     print('All IPs of active interface:');
     for (var ipInfo in activeIPs) {
       print('Type: ${ipInfo[0]}, Address: ${ipInfo[1]}, Private: ${ipInfo[2]}');
-      print('Public IP: ${ipInfo[3]},Public Port: ${ipInfo[4]} NATed: ${ipInfo[5]}, NAT Type: ${ipInfo[6]}');
+      print('Public IP: ${ipInfo[3]},Public Port: ${ipInfo[4]} NATed: ${ipInfo[5]}, NAT Type: ${ipInfo[6]}, Public IPv6:${ipInfo[7]}');
     }
   }
+ 
+    Map<String, int> servers = await NetworkInfo.loadServersFromJson('servers.json');
+  if (servers.isEmpty) {
+    print('No servers found in JSON file.');
+    return;
+  }
+
+  String closestServer = await NetworkInfo.findClosestServer(servers);
+  print('Closest server: $closestServer');
   await networkInfo.startListening(port: 8888);
 
 }
